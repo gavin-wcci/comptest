4dddbd6a30079edd2459a899302e97fb743589aa
Gavin <97972972+gavin-wcci@users.noreply.github.com>
2022-09-09 11:49:28 -0400

    Initial commit

---
+++ Exercises.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace template_csharp_computational_thinking
+{
+    internal class Exercises
+    {
+        public static void AreTheseIntegersEqual()
+        {
+            // Ask the user for two integers. 
+            // Check and see if the two integers are equal to each other.
+            // If they are, inform the the user that the numbers are equal,
+            // else inform the user that the numbers are not equal
+
+            Console.Clear();
+            Console.WriteLine("ARE THESE INTEGERS EQUAL?\n");
+
+            // Enter your solution here
+
+            Console.Write("Press enter to return to the Main Menu");
+            Console.ReadLine();
+        }
+
+        public static void EvenOrOdd()
+        {
+            // Ask the user for a number.
+            // Tell the user if that number is even or odd.
+
+            Console.Clear();
+            Console.WriteLine("EVEN OR ODD?\n");
+
+            // Enter your solution here
+
+            Console.Write("Press enter to return to the Main Menu");
+            Console.ReadLine();
+        }
+
+        public static void VowelOrConsonant()
+        {
+            // Ask the user for a letter. Check whether a letter is a vowel or consonant.
+            // Hint: You can do this problem using a switch or an if condition.
+
+            Console.Clear();
+            Console.WriteLine("VOWEL OR CONSONANT?\n");
+
+            // Enter your solution here
+
+            Console.Write("Press enter to return to the Main Menu");
+            Console.ReadLine();
+        }
+
+        public static void FindLargestNumber()
+        {
+            // Prompt the user to input two integer values.
+            // Find and print the greatest value of the two integers.
+
+            Console.Clear();
+            Console.WriteLine("FIND THE LARGEST NUMBER\n");
+
+            // Enter your solution here
+
+            Console.Write("Press enter to return to the Main Menu");
+            Console.ReadLine();
+        }
+
+        public static void DivisibleBy3()
+        {
+            // Ask the user for a number and then tell them if it is divisible by 3
+
+            Console.Clear();
+            Console.WriteLine("DIVISIBLE BY 3\n");
+
+            // Enter your solution here
+
+            Console.Write("Press enter to return to the Main Menu");
+            Console.ReadLine();
+        }
+
+        public static void WordsToDigits()
+        {
+            // Having a string representation of a number, you need to print the digit form of the number.
+            // Ask the user to enter a string representation of a number from zero to ten.
+            // Using switch case, print the digit (0-10) representation of the number.
+
+            Console.Clear();
+            Console.WriteLine("WORDS TO DIGITS\n");
+
+            // Enter your solution here
+
+            Console.Write("Press enter to return to the Main Menu");
+            Console.ReadLine();
+        }
+
+        public static void WhichNameIsLonger()
+        {
+            // Ask the user for their first and last name.
+
+            // Print the user's full name.
+            // If the first name is longer than the last name, print "First is longer."
+            // If the first name and last name are the same length, print "Same-sies!"
+            // Otherwise, print "Last must be longer!"
+
+            Console.Clear();
+            Console.WriteLine("WHICH NAME IS LONGER?\n");
+
+            // Enter your solution here
+
+            Console.Write("Press enter to return to the Main Menu");
+            Console.ReadLine();
+        }
+
+        public static void AreNamesSame()
+        {
+            // Ask the user for 2 names.
+
+            // If the two names are the same, print "The names are the same."
+            // If they're not the same, print "The names are different."
+
+            Console.Clear();
+            Console.WriteLine("ARE THESE NAMES THE SAME?\n");
+
+            // Enter your solution here
+
+            Console.Write("Press enter to return to the Main Menu");
+            Console.ReadLine();
+        }
+
+        public static void NameAndBirthplace()
+        {
+            // Ask the user for their first name and where they were born.
+            // Print a sentence to the console that repeats this information.
+
+            Console.Clear();
+            Console.WriteLine("NAME AND PLACE OF BIRTH\n");
+
+            // Enter your solution here
+
+            Console.Write("Press enter to return to the Main Menu");
+            Console.ReadLine();
+        }
+
+
+        //LOOPS SECTION
+        //For this next section, in order to get a passing grade, each of the following problems must be solved using at least one of the the loop types we learned in class.
+
+        public static void ReverseCounting()
+        {
+            // Ask the user for a number between 1-20. Starting at the number given,
+            // count backwards to 1 and print the numbers to the console.
+
+            Console.Clear();
+            Console.WriteLine("REVERSE COUNTING\n");
+
+            // SOLUTION HERE
+
+            Console.Write("Press enter to return to the Main Menu");
+            Console.ReadLine();
+        }
+
+        public static void SquareOfEvenNumbers()
+        {
+            // For the numbers 1-9, if a number is even print the square of that number (number multiplied by itself) and if it is odd, just print the number
+
+            Console.Clear();
+            Console.WriteLine("PRINT THE SQUARE OF EVEN NUMBERS\n");
+
+            // SOLUTION HERE
+
+            Console.Write("Press enter to return to the Main Menu");
+            Console.ReadLine();
+        }
+
+        public static void GuessMagicNumber()
+        {
+            // Write a console application that asks the user for an integer.
+            // If that integer is evenly divisible by 3, then print “You Won!”.
+            // If it isn’t, ask the user to “Try again.”
+            // Keep asking them for a number (looping) until they win.
+
+            Console.Clear();
+            Console.WriteLine("GUESS THE MAGIC NUMBER\n");
+
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            // This reads the user's input and converts it to an integer.
+            // We'll assume that users can only input integers.
+            // SOLUTION HERE
+
+            Console.Write("Press enter to return to the Main Menu");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StretchMe.md
@@ -0,0 +1,27 @@
+﻿# WCCI - Computational Thinking
+## Stretch Tasks
+
+> The name of this game is: SCOPE
+
+- We need to add a counter to the Program class to keep track of how many of the system commands are return
+- We also need to keep track of the last program that was ran so that we can provide an exit report
+- An exit report should inform the user how many total system commands were ran as well as what the last command was
+
+## Tips and Tricks
+
+- Declare a static variable to keep track of the count of system command runs
+- Add increment statements at the appropriate locations of the Main method
+- Declare a static variable to keep track of the name of the last system command
+
+## Hints
+
+- You have a couple of options, you can create static variables at the Program level or you can create local variables at the Main method level
+
+## Sample Code
+
+```chsarp
+int count = 15;
+count++; // adds 1
+count += 1; // adds 1
+count = count + 1; // adds 1
+Console.WriteLine(count); // outputs 18
